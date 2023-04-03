e42cf5267000b7d522181504134b1f3a9d5e23a5
HYGOR VINICIUS COSTA SILVA <hygorvcosta@gmail.com>
2023-04-03 13:35:20 -0300

    Add: Receita e Óleo Equivalente
    Calcular o óleo equivalente e a receita trimestral para análise da participação especial.

--- producao_trimestral.py
+++ producao_trimestral.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 from datetime import datetime
@@ -5,29 +6,44 @@ from dateutil.relativedelta import relativedelta
 
 class Producao:
 
-    def __init__(self, producao_excel) -> None:
-        self.df = pd.read_excel(producao_excel, skiprows = range(0, 2))
-        self.df.columns = ['Tempo (dias)', 'Data', 'Oil-PROD - Trimestral (Mm3)', 'Water-PROD - Trimestral (Mm3)', 'Gas-PROD - Trimestral (Mm3)', 'Water-INJ - Trimestral (Mm3)']
-        self.df['Data'] = pd.to_datetime(self.df['Data'])
+    def __init__(self, producao_excel, dados) -> None:
+        self._split_name = os.path.splitext(producao_excel)
+        self.df = self.configurar_producao(producao_excel)
+        self.price = pd.read_excel(dados, sheet_name='Stock_Oil_Price', index_col=0)
         self.datas = self.df['Data']
+        self.prod_trimestral = self.producao_trimestral()
+        self.calcular_receita_trimestral()
 
-    def producao_trimestral(self, coluna):
+    def configurar_producao(self, producao_excel):
+        prod = pd.read_excel(producao_excel, skiprows = range(0, 2))
+        prod.columns = ['Tempo (dias)', 'Data', 'Oil-PROD - Trimestral (Mm3)', 'Water-PROD - Trimestral (Mm3)', 'Gas-PROD - Trimestral (Mm3)', 'Water-INJ - Trimestral (Mm3)']
+        prod['Data'] = pd.to_datetime(prod['Data'])
+        return prod
+
+    def agrupar_trimestral(self, coluna):
         return self.df.groupby(self.df['Data'].dt.to_period('Q'))[coluna].sum()/1000
-
+    
     def shift_values(self):
         self.df = self.df.shift(-1)
         self.df['Data'] = self.datas
         self.df.fillna(0)
-
-    def write_file(self):
-        df_trimestral = pd.DataFrame()
+    
+    def producao_trimestral(self):
+        prod_tri = pd.DataFrame()
         self.shift_values()
         for col in self.df.columns[2:]: 
-            df_trimestral[col] = self.producao_trimestral(col)
-        df_trimestral.to_excel("Producao_Trimestral.xlsx")
+            prod_tri[col] = self.agrupar_trimestral(col)
+        prod_tri['Óleo_Equiv_(Mm3)'] = prod_tri['Oil-PROD - Trimestral (Mm3)'] + prod_tri['Gas-PROD - Trimestral (Mm3)'] / 1017.5321
+        return prod_tri
+    
+    def calcular_receita_trimestral(self):
+        prod_oleo = self.prod_trimestral['Oil-PROD - Trimestral (Mm3)']
+        prod_gas = self.prod_trimestral['Gas-PROD - Trimestral (Mm3)']
+        price = self.price['Base'].repeat(4).reset_index()
+        price = price.drop(price.tail(3).index)
+        price = price.set_index(prod_oleo.index)
+        self.prod_trimestral['Receita']  = price['Base']*6.29*1_000*prod_oleo + (4*37.31/1000)*prod_gas
 
-
-#################################
-if __name__ == '__main__':
-    prod = Producao('Pituba_Down_Mensal.xlsx')
-    prod.write_file()
+    def write_file(self):
+        output_name = self._split_name[0] + '_Trimestral' + self._split_name[1]
+        self.prod_trimestral.to_excel(output_name)
