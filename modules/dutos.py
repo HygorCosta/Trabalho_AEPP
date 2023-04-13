165775587b602b98942099b9448289671137343d
HYGOR VINICIUS COSTA SILVA <hygorvcosta@gmail.com>
2023-04-13 08:38:15 -0300

    Mod: Dutos - add: select flowline

--- modules/dutos.py
+++ modules/dutos.py
@@ -8,39 +8,29 @@ class Dutos:
     def __init__(self, config, tipo: str) -> None:
         self.dados = self.read(config)
         self.tipo = tipo # 'A'-flexível, 'B'-rígido
+        self.capex_flow, self.opex_flow = self.select_flowline()
         
     def read(self, config):
         with open(config, 'r') as f:
             dados = yaml.safe_load(f)
         return dados
+    
+    def select_flowline(self):
+        if self.tipo == '4A':
+            return FlowlineFlexivel(self.dados), OpexFlexivel(self.dados)
+        elif self.tipo == '4B':
+            return FlowlineRigido(self.dados), OpexRigido(self.dados)
+        else:
+            raise ValueError(f'Parâmetro {self.tipo} eh invalido, apenas A para flexivel e B para rigido.')
 
     def capex(self):
-        if self.tipo == '4A':
-            umbilical = Umbilical(self.dados).capex()
-            gaslift = GasLift(self.dados).capex()
-            riser = Riser(self.dados).capex()
-            flowline_flexivel = FlowlineFlexivel(self.dados).capex()
-            return umbilical + gaslift + riser + flowline_flexivel
-        elif self.tipo == '4B':
-            umbilical = Umbilical(self.dados).capex()
-            gaslift = GasLift(self.dados).capex()
-            riser = Riser(self.dados).capex()
-            return FlowlineRigido(self.dados).capex()
-        else:
-            raise ValueError(f'Parâmetro {self.tipo} eh invalido, apenas A para flexivel e B para rigido.')
+        umbilical = Umbilical(self.dados).capex()
+        gaslift = GasLift(self.dados).capex()
+        riser = Riser(self.dados).capex()
+        return umbilical + gaslift + riser + self.capex_flow.capex()
         
     def opex(self):
-        if self.tipo == '4A':
-            return OpexFlexivel(self.dados).opex()
-        elif self.tipo == '4B':
-            return OpexRigido(self.dados).opex()
-        else:
-            raise ValueError(f'Parâmetro {self.tipo} eh invalido, apenas A para flexivel e B para rigido.')
+        return self.opex_flow.opex()
         
     def descomissionamento(self):
-        if self.tipo == '4A':
-            return OpexFlexivel(self.dados).opex_descomissionamento()
-        elif self.tipo == '4B':
-            return OpexRigido(self.dados).opex_descomissionamento()
-        else:
-            raise ValueError(f'Parâmetro {self.tipo} eh invalido, apenas A para flexivel e B para rigido.')
+        return self.opex_flow.opex_descomissionamento()
