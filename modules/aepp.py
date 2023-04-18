dde76637ba4bc2893cddf69e7c415d5fd878089e
HYGOR VINICIUS COSTA SILVA <hygorvcosta@gmail.com>
2023-04-18 20:20:42 -0300

    Feature - Finish: Indicadores Eco

--- modules/aepp.py
+++ modules/aepp.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy_financial as npf
 import numpy as np
 from .caixa import Caixa
+from scipy.optimize import root_scalar
 
 
 class AEPP:
-
     def __init__(self, caixa) -> None:
         self.caixa = caixa
+        self.preco_atual = self.caixa.prod.price[self.caixa.modelo].values[0]
+        self.price_vp_zero = self._find_oil_price_vpl_null()
 
     def vpl(self):
         return self.caixa()
@@ -33,30 +35,18 @@ class AEPP:
         f = self._receitas_futuro()
         i = self._dispendios_presente()
         n = len(self.caixa.cash_flow()) - 1
-        return (f/i)**(1/n) - 1
-    
-    def _receitas_presente(self):
-        cf = self.caixa.cash_flow().values
-        cf[cf < 0] = 0
-        cf = np.insert(cf, 0, 0)
-        return npf.npv(self.caixa.tma, cf)
+        return (f / i) ** (1 / n) - 1
 
     def il(self):
-        receitas = self._receitas_presente()
-        disp = self._dispendios_presente()
-        return receitas/disp
+        receitas = self._receitas_total_presente()
+        disp = self._dispendios_total_presente()
+        return receitas / disp
 
     def roi(self):
-        capex = self.caixa.capex().values
-        capex = np.insert(capex, 0, 0)
-        capex_pres = npf.npv(self.caixa.tma, capex)
-        return self.caixa.vpl() / capex_pres
+        return self.caixa.vpl() / self.capex_vp
 
     def lu(self):
-        receita_pres = self._receitas_presente()
-        price = self.caixa.prod.price.values[0][0]
-        vp_pec = receita_pres / price
-        return self.caixa.vpl() / vp_pec
+        return self.caixa.vpl() / self.vp_pec
 
     def _fun_cup(self, x):
         self.prod.price.iloc[:] = x
@@ -66,52 +56,110 @@ class AEPP:
         return pd.Series(self.vpl())
 
     def _find_oil_price_vpl_null(self):
-        or_price = self.prod.price
-        x0 = self.prod.price.mean()['Base']
-        _ = self._fun_cup(x0)
-        raiz = root(self._fun_cup, x0)
-        self.price_vpl_null = raiz.x
-        self._update_model(raiz.x)
+        root = root_scalar(self.caixa, x0=20, x1=100).root
+        self.caixa.restore_model()
+        return root
 
-    def _update_model(self, price):
-        self.prod.price.iloc[:] = price
-        self.receitas = self.total_revenue()
-        self.capex_prod = self.capex_producao()
-        self.despesas = self.total_cost()
+    def vpl_zero_model(self):
+        self.caixa.prod.price[self.caixa.modelo] = self.price_vp_zero
+        self.caixa.update_model()
 
-    def _receitas_vpl_zero(self):
-        self._update_model(self.price_vpl_null)
-        receitas_pe = self.receitas.to_numpy()
-        return receitas_pe
+    def _capex(self):
+        capex = self.caixa.capex()
+        descom = self.caixa.despesas["descom"]
+        return capex + descom
 
-    def _dispendios_vpl_zero(self):
-        self._update_model(self.price_vpl_null)
-        disp = self._global_dispendios_presente()
-        return disp
+    def _get_vp(self, df):
+        df = np.insert(df.values, 0, 0)
+        capex_pres = npf.npv(self.caixa.tma, df)
+        return capex_pres
+
+    @property
+    def capex_vp(self):
+        return self._get_vp(self._capex())
+
+    @property
+    def opex_vp(self):
+        return self._get_vp(self._opex())
+
+    @property
+    def tributo_vp(self):
+        return self._get_vp(self._tributacao())
+
+    def _opex(self):
+        if self.caixa.tarefa in ("4A", "4B"):
+            opex = self.caixa.despesas[["opex_var", "opex_fixo", "opex_p16"]].sum(
+                axis=1
+            )
+        else:
+            opex = self.caixa.despesas[["opex_var", "opex_fixo"]].sum(axis=1)
+        return opex
+
+    def _tributacao(self):
+        imposto = self.caixa.despesas["imposto"]
+        ir_csll = self.caixa.net_profit_tax()
+        return imposto + ir_csll
+
+    def _dispendios_total_presente(self):
+        capex = self._capex()
+        opex = self._opex()
+        tributo = self._tributacao()
+        disp = (capex + opex + tributo).values
+        disp = np.insert(disp, 0, 0)
+        disp_pres = npf.npv(self.caixa.tma, disp)
+        return disp_pres
+
+    def _receitas_total_presente(self):
+        receitas = self.caixa.receitas.values
+        receitas = np.insert(receitas, 0, 0)
+        receitas_pres = npf.npv(self.caixa.tma, receitas)
+        return receitas_pres
+
+    @property
+    def vp_pec(self):
+        receita_pres = self._receitas_total_presente()
+        price = self.caixa.prod.price.values[0][0]
+        vp_pec = receita_pres / price
+        return vp_pec
 
     def cup(self):
-        if self.price_vpl_null:
-            self._find_oil_price_vpl_null()
-        receita_pres = npf.npv(self.tma, self.receitas.to_numpy())
-        vp_pec = receita_pres / self.or_vpl_null
-        disp = self._dispendios_vpl_zero()
-        self._restore_original()
-        return float(disp / vp_pec)
+        return self.price_vp_zero
 
     def cui(self):
-        if self.price_vpl_null:
-            self._find_oil_price_vpl_null()
-        receita_pres = npf.npv(self.tma, self.receitas.to_numpy())
-        vp_capex = npf.npv(self.tma, self.capex().to_numpy())
-        vp_pec = receita_pres / self.or_vpl_null
-        self._restore_original()
-        return float(vp_capex / vp_pec)
+        self.vpl_zero_model()
+        custo_unit_inv = self.capex_vp / self.vp_pec
+        self.caixa.restore_model()
+        return custo_unit_inv
 
     def cuo(self):
-        pass
+        self.vpl_zero_model()
+        custo_unit_oper = self.opex_vp / self.vp_pec
+        self.caixa.restore_model()
+        return custo_unit_oper
 
     def cut(self):
-        pass
+        self.vpl_zero_model()
+        custo_unit_trib = self.tributo_vp / self.vp_pec
+        self.caixa.restore_model()
+        return custo_unit_trib
 
     def hub_indicadores_economicos(self):
-        pass
+        names = ["vpl", "tir", "tgr", "il", "roi", "lu", "cup", "cui", "cuo", "cut"]
+        indicadores = pd.DataFrame(columns=names)
+        indicadores.loc[0] = [
+            self.caixa.vpl(),
+            self.tir(),
+            self.tgr(),
+            self.il(),
+            self.roi(),
+            self.lu(),
+            self.cup(),
+            self.cui(),
+            self.cuo(),
+            self.cut(),
+        ]
+        indicadores = indicadores.stack()
+        indicadores.index.names = ['zero', 'param']
+        indicadores.index = indicadores.index.get_level_values('param')
+        return indicadores
+
--- test/test_aepp.py
+++ test/test_aepp.py
@@ -2,26 +2,63 @@ from modules.caixa import Caixa
 from modules.aepp import AEPP
 import pytest
 
+
 def create_projeto():
-    tarefa_01 = Caixa(tarefa='1', modelo='Up')
+    tarefa_01 = Caixa(tarefa="1", modelo="Up")
     return AEPP(tarefa_01)
+
 
 def test_dispendios_presente():
     aepp = create_projeto()
     aepp._dispendios_presente()
 
-def test_dispendios_presente():
+
+def test__receitas_futuro():
     aepp = create_projeto()
     aepp._receitas_futuro()
 
-def test_dispendios_presente():
+
+def test_tgr():
     aepp = create_projeto()
     aepp.tgr()
 
-def test_dispendios_presente():
+
+def test_roi():
     aepp = create_projeto()
     aepp.roi()
 
-def test_dispendios_presente():
+
+def test_lu():
     aepp = create_projeto()
     aepp.lu()
+
+
+def test_dispendios_presente():
+    aepp = create_projeto()
+    aepp._find_oil_price_vpl_null()
+
+
+def test_dispendios_cup():
+    aepp = create_projeto()
+    aepp.cup()
+
+
+def test_dispendios_cuo():
+    aepp = create_projeto()
+    aepp.cuo()
+
+
+def test_dispendios_cui():
+    aepp = create_projeto()
+    aepp.cui()
+
+
+def test_dispendios_cup_cuo_cui_cut():
+    aepp = create_projeto()
+    obtido = aepp.cuo() + aepp.cui() + aepp.cut()
+    assert pytest.approx(aepp.cup()) == obtido
+
+
+def test_dispendios_hub():
+    aepp = create_projeto()
+    aepp.hub_indicadores_economicos()
