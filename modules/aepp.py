d9051c8c3d91cd8ebdf870e4fe2fb413c47509fb
HYGOR VINICIUS COSTA SILVA <hygorvcosta@gmail.com>
2023-04-23 20:10:44 -0300

    Fix: IL

--- modules/aepp.py
+++ modules/aepp.py
@@ -38,8 +38,8 @@ class AEPP:
         return (f / i) ** (1 / n) - 1
 
     def il(self):
-        receitas = self._receitas_total_presente()
-        disp = self._dispendios_total_presente()
+        receitas = self.caixa.discounted_cash_flow()[self.caixa.discounted_cash_flow() > 0].sum()
+        disp = - self.caixa.discounted_cash_flow()[self.caixa.discounted_cash_flow() < 0].sum()
         return receitas / disp
 
     def roi(self):
