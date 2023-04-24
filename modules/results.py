696148b47e3194c71406bf6d66f597ad194aacc4
HYGOR VINICIUS COSTA SILVA <hygorvcosta@gmail.com>
2023-04-24 19:17:01 -0300

    Send hub financial to Caixa

--- modules/results.py
+++ modules/results.py
@@ -7,27 +7,11 @@ class Results:
     def __init__(self, projeto, parametros_eco) -> None:
         self.projeto = projeto
         self.parametros_eco = parametros_eco
-        self.hub = self._hub_financial()
+        self.hub = self.projeto._hub_financial()
         self.financiamento = self.projeto.payment_loan_price()
         if self.projeto.tarefa in ('4A', '4B'):
             self.hub_p16 = projeto.dutos.hub_opex
 
-
-    def _hub_financial(self):
-        receita = self.projeto.receitas.rename('receita')
-        despesas = self.projeto.despesas
-        lucro_bruto = self.projeto.lucro_bruto().rename('lucro_bruto')
-        deprec = self.projeto.depreciacao().rename('depreciacao')
-        residual = self.projeto.valor_residual().rename('residual')
-        lucro_trib = self.projeto.net_income_before_tax().rename('lucro_tributavel')
-        ir = self.projeto.net_profit_tax().rename('ir_csll')
-        lucro_liq = self.projeto.net_income_after_tax().rename('lucro_liquido')
-        capex = self.projeto.capex().rename('capex_prod')
-        flc = self.projeto.cash_flow().rename('cash_flow')
-        flc_disc = self.projeto.discounted_cash_flow()
-        data_frames = [receita, despesas, lucro_bruto, deprec, residual, lucro_trib, ir, lucro_liq, capex, flc, flc_disc] 
-        return pd.concat(data_frames, join='outer', axis=1)
-    
     def _set_spreadsheet_format_prod_trimestral(self, writer):
         workbook = writer.book
         worksheet = writer.sheets['Prod_Tri_PE']
