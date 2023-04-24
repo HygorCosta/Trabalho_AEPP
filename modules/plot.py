22c3202d2772ffd8f90c4d53a3075d73c2539899
HYGOR VINICIUS COSTA SILVA <hygorvcosta@gmail.com>
2023-04-24 19:16:46 -0300

    Feat: Barplot Cash Flow

--- modules/plot.py
+++ modules/plot.py
@@ -2,48 +2,87 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from modules.caixa import Caixa
+
 plt.rcParams["figure.figsize"] = [10, 6]
-sns.set( style = "darkgrid" )  
+sns.set(style="darkgrid")
+
 
 class PPlot:
-    def __init__(self, lista_projetos: list, names: list) -> None:
-        self.lista_projetos = lista_projetos
-        self.names = names
-
-    def cumulative_production(self):
-        producao = [proj.prod.prod_anual for proj in self.lista_projetos]
-        for project, name in zip(producao, self.names):
-            project['oil_prod_cum'] = project['oil_prod'].cumsum()
-            project['projeto'] = name
-        pp = pd.concat(producao)
-        pp['date'] = np.tile(np.arange(2024, 2054), len(self.names)) 
-        return pp
+    def __init__(self, projeto: Caixa) -> None:
+        self.projeto = projeto
+        self.df = projeto._hub_financial()
 
     def plot_cumulative_production(self):
+        # TODO: Fix cumulative production
         fig, ax = plt.subplots()
         producao_acumulada = self.cumulative_production()
-        axes = sns.lineplot(x='date', y='oil_prod_cum', data=producao_acumulada, hue='projeto', style='projeto', markers=True, ax=ax)
-        axes.set(xlabel='Anos', ylabel='Produção acumulada ($m^3$)')
-        ax2 = plt.axes([.6, .2, .3, .3], xlim=[2044, 2053], ylim=[4e8, 5.2e8])
-        axes2 = sns.lineplot(x='date', y='oil_prod_cum', data=producao_acumulada, hue='projeto', style='projeto', markers=True, ax=ax2)
-        axes2.set_ylabel('')
-        axes2.set_xlabel('')
+        axes = sns.lineplot(
+            x="date",
+            y="oil_prod_cum",
+            data=producao_acumulada,
+            hue="projeto",
+            style="projeto",
+            markers=True,
+            ax=ax,
+        )
+        axes.set(xlabel="Anos", ylabel="Produção acumulada ($m^3$)")
+        ax2 = plt.axes([0.6, 0.2, 0.3, 0.3], xlim=[2044, 2053], ylim=[4e8, 5.2e8])
+        axes2 = sns.lineplot(
+            x="date",
+            y="oil_prod_cum",
+            data=producao_acumulada,
+            hue="projeto",
+            style="projeto",
+            markers=True,
+            ax=ax2,
+        )
+        axes2.set_ylabel("")
+        axes2.set_xlabel("")
         plt.legend([], [], frameon=False)
-        axes2.set_title('zoom')
+        axes2.set_title("zoom")
         axes2.set_xlim([2045, 2053])
         plt.tight_layout()
         plt.show()
 
-        
+    @property
+    def receitas(self):
+        return self.df.receita.rename("Receitas")
 
-    def net_cash_split(self):
-        pass
+    @property
+    def capex(self):
+        return -self.df.capex_prod.rename("CAPEX")
 
-    def net_cash_flows(self):
-        pass
+    @property
+    def dispendios(self):
+        # OPEX + GT
+        opex = self.df[["opex_var", "opex_fixo"]].sum(axis=1)
+        gt = self.df.imposto + self.df.ir_csll
+        return -(opex + gt).rename("OPEX + GT")
 
-    def npv_sensitivity(self):
-        pass
+    @property
+    def abex(self):
+        # descomissionamento
+        return -self.df.descom.rename("ABEX")
 
-    def npv_profile(self):
-        pass
+    @property
+    def disc_cash_flow(self):
+        return self.df.disc_cash_flow.rename("FLC (nominal)")
+
+    def barplot_cash_flow(self):
+        df = pd.concat(
+            [self.receitas, self.capex, self.dispendios, self.abex],
+            join="outer",
+            axis=1,
+        )
+        fig, ax = plt.subplots()
+        df.plot(kind="bar", stacked=True, ax=ax, alpha=.7)
+        self.disc_cash_flow.plot(
+            kind="line", ax=ax, use_index=False, color="black", lw=2
+        )
+        ax.set_xlabel("Anos")
+        ax.set_ylabel("Valor Monetário - US$")
+        plt.xticks(rotation=90)
+        plt.legend()
+        file_name = self.projeto.prod._file_name
+        plt.savefig("out/" + file_name[0] + "_barplot_cash_flow.png")
--- test/test_plot.py
+++ test/test_plot.py
@@ -5,31 +5,52 @@ from modules.results import Results
 from modules.aepp import AEPP
 from modules.plot import PPlot
 
+
 def create_obj():
-    tarefa_01 = Caixa(tarefa='1', modelo='Up')
+    tarefa_01 = Caixa(tarefa="1", modelo="Up")
     # Tarefas 02 - Down:
     tarefa_02_down = Caixa(
-    tarefa='2', dados_producao='producao/Pituba_Down_Mensal.xlsx', modelo='Down')
+        tarefa="2", dados_producao="producao/Pituba_Down_Mensal.xlsx", modelo="Down"
+    )
     # Tarefas 02 - Base:
     tarefa_02_base = Caixa(
-    tarefa='2', dados_producao='producao/Pituba_Base_Mensal.xlsx')
+        tarefa="2", dados_producao="producao/Pituba_Base_Mensal.xlsx"
+    )
     # Tarefa 03:
-    tarefa_03 = Caixa(tarefa='3', dados_producao='producao/Pituba_Base_LS.xlsx')
+    tarefa_03 = Caixa(tarefa="3", dados_producao="producao/Pituba_Base_LS.xlsx")
     # Tarefa 04 - Flexivel:
-    tarefa_04_flex = Caixa(
-    tarefa='4A', dados_producao='producao/Producao_P16F.xlsx')
+    tarefa_04_flex = Caixa(tarefa="4A", dados_producao="producao/Producao_P16F.xlsx")
     # Tarefa 04 - Rigido:
-    tarefa_04_rigido = Caixa(
-    tarefa='4B', dados_producao='producao/Producao_P16R.xlsx')
-# Tarefas 02 - Up:
+    tarefa_04_rigido = Caixa(tarefa="4B", dados_producao="producao/Producao_P16R.xlsx")
+    # Tarefas 02 - Up:
     tarefa_02_up = Caixa(
-    tarefa='2', dados_producao='producao/Pituba_Up_Mensal.xlsx',
-    modelo='Up')
-    lista_projetos = [tarefa_02_down, tarefa_02_base, tarefa_02_up, tarefa_03, tarefa_04_flex, tarefa_04_rigido]
-    nome_projetos = ['tarefa_02_down', 'tarefa_02_base', 'tarefa_02_up', 'tarefa_03', 'tarefa_04_flex', 'tarefa_04_rigido']
+        tarefa="2", dados_producao="producao/Pituba_Up_Mensal.xlsx", modelo="Up"
+    )
+    lista_projetos = [
+        tarefa_02_down,
+        tarefa_02_base,
+        tarefa_02_up,
+        tarefa_03,
+        tarefa_04_flex,
+        tarefa_04_rigido,
+    ]
+    nome_projetos = [
+        "tarefa_02_down",
+        "tarefa_02_base",
+        "tarefa_02_up",
+        "tarefa_03",
+        "tarefa_04_flex",
+        "tarefa_04_rigido",
+    ]
     return PPlot(lista_projetos, names=nome_projetos)
 
 
 def test_cumulative_production():
     projeto = create_obj()
     projeto.plot_cumulative_production()
+
+
+def test_barplot_cash_flow():
+    projeto = Caixa(tarefa="1", modelo="Up")
+    pplot = PPlot(projeto)
+    pplot.barplot_cash_flow()
